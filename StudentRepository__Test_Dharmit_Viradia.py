c75412c13715b2cced6ad71281fdff2c18677040
Dharmit Viradia <dharmit.viradia@gmail.com>
2020-04-08 16:09:13 -0400

    Changed to HW10

--- StudentRepository__Test_Dharmit_Viradia.py
+++ StudentRepository__Test_Dharmit_Viradia.py
@@ -1,32 +1,225 @@
-"""" Test for Repository """
+""" Creating a Data Repository for University """
+
+import os
+from collections import defaultdict
+from prettytable import PrettyTable
+from typing import Dict, DefaultDict, Tuple, List, Iterator, Set
+from HW08_Dharmit_Viradia import file_reader
+
+
+class Repository:
+    "Major, Student and Instructor repository"
+
+    def __init__(self, path: str, tables: bool = True) -> None:
+        self._path: str = path
+        self._students: Dict[str, Student] = dict()
+        self._instructors: Dict[str, Instructor] = dict()
+        self._majors: Dict[str, Major] = dict()
+
+        try:
+            self._get_majors(os.path.join(path, 'majors.txt'))
+            self._get_students(os.path.join(path, 'students.txt'))
+            self._get_instructors(os.path.join(path, 'instructors.txt'))
+            self._get_grades(os.path.join(path, 'grades.txt'))
+
+        except (FileNotFoundError, ValueError) as e:
+            print(e)
+
+        else:
+            if tables:
+                print("\nMajors Table")
+                self.majors_table()
+                print("\nStudent Table ")
+                self.student_table()
+                print("\nInstructor Table ")
+                self.instructor_table()
+
+    def _get_majors(self, path: str):
+        """Major details are read using file reading gen and added to dictionary"""
+        for major, flag, course in file_reader(path, 3, sep='\t', header=True):
+            if major not in self._majors:
+                self._majors[major] = Major(major)
+            self._majors[major].add_course(course, flag)
+
+    def _get_students(self, path: str) -> None:
+        """ Student detail are read using file reading gen and added to dictionary """
+        for cwid, name, major in file_reader(path, 3, sep=';', header=True):
+            if major not in self._majors:
+                print(f"Student {cwid} '{name}' has unknown major '{major}'")
+            else:
+                self._students[cwid] = Student(cwid, name, self._majors[major])
+
+    def _get_instructors(self, path: str) -> None:
+        """ Instructor detail are read using file reading gen and added to dictionary """
+        for cwid, name, dept in file_reader(path, 3, sep='|', header=True):
+            self._instructors[cwid] = Instructor(cwid, name, dept)
+
+    def _get_grades(self, path: str) -> None:
+        """Grades are read using file reading gen and assigned to student and instructor """
+        for std_cwid, course, grade, instructor_cwid in file_reader(path, 4, sep='|', header=True):
+            if std_cwid in self._students:
+                self._students[std_cwid].add_course(course, grade)
+            else:
+                print(f'Grades for student is {std_cwid}')
+
+            if instructor_cwid in self._instructors:
+                self._instructors[instructor_cwid].add_student(course)
+            else:
+                print(f'Grades for instructor is {instructor_cwid}')
+
+    def majors_table(self) -> None:
+        """ Majors table """
+        table: PrettyTable = PrettyTable(field_names=Major.FIELD_NAMES)
+        for major in self._majors.values():
+            table.add_row(major.info())
+        print(table)
+
+    def student_table(self) -> None:
+        """ Student table """
+        table: PrettyTable = PrettyTable(field_names=Student.FIELD_NAMES)
+        x: List[str] = list()
+        for student in self._students.values():
+            table.add_row(student.info())
+            x.append(student.info())
+        print(table)
+        # print(x)
+
+    def instructor_table(self) -> None:
+        """ Instructor table """
+        table: PrettyTable = PrettyTable(field_names=Instructor.FIELD_NAMES)
+        for instructor in self._instructors.values():
+            for row in instructor.info():
+                table.add_row(row)
+        print(table)
+
+
+class Major:
+    """ Major Class """
+    FIELD_NAMES: List[str] = ['Major', 'Required Courses', 'Electives']
+    GRADES: Set[str] = {'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C'}
+
+    def __init__(self, major: str) -> None:
+        self._major: str = major
+        self._required: Set[str] = set()
+        self._electives: Set[str] = set()
+
+    def add_course(self, course: str, option: str) -> None:
+        if option == 'R':
+            self._required.add(course)
+        elif option == 'E':
+            self._electives.add(course)
+        else:
+            raise ValueError("Course Flag not Valid")
+
+    def remaining(self, completed: Dict[str, str]) -> Tuple[str, List[str], List[str], List[str]]:
+        """Adding remaining courses as well as electives"""
+        passed: Set[str] = {course for course,
+                            grade in completed.items() if grade in Major.GRADES}
+        remaining: Set[str] = self._required - passed
+        electives: Set[str] = self._electives
+
+        if self._electives.intersection(passed):
+            electives = []
+
+        return self._major, list(passed), list(remaining), list(electives)
+
+    def info(self) -> Tuple[str, List[str], List[str]]:
+        """ Returning a row in table """
+        return [self._major, sorted(self._required), sorted(self._electives)]
+
+
+class Student:
+    """Student Class """
+    FIELD_NAMES: List[str] = ['CWID', 'Name', 'Major',
+                              'Completed Courses', 'Remaining Required', 'Remaining Electives', 'GPA']
+
+    def __init__(self, cwid: str, name: str, major: str) -> None:
+        self._cwid: str = cwid
+        self._name: str = name
+        self._major: str = major
+        self._courses: Dict[str, str] = dict()
+
+    def add_course(self, course: str, grade: str) -> None:
+        """ Adding course with grade """
+        self._courses[course] = grade
+
+    def gpa(self) -> float:
+        """Calculate the GPA using dictionary"""
+        grades: Dict[str, float] = {'A': 4.0, 'A-': 3.75, 'B+': 3.25, 'B': 3.0, 'B-': 2.75,
+                                    'C+': 2.25, 'C': 2.0, "C-": 0.00, "D+": 0.00, "D": 0.00, "D-": 0.00, "F": 0.00}
+        try:
+            total: float = sum(
+                [grades[grade] for grade in self._courses.values()]) / len(self._courses.values())
+            return round(total, 2)
+        except ZeroDivisionError as e:
+            print(e)
+
+    def info(self) -> Tuple[str, str, str, List[str], List[str], List[str]]:
+        """ return a list of information needed for pretty table """
+        major, passed, remaining, electives = self._major.remaining(
+            self._courses)
+
+        return [self._cwid, self._name, major, sorted(passed), sorted(remaining), sorted(electives), self.gpa()]
+
+
+class Instructor:
+    """ Instructor class """
+    FIELD_NAMES: List[str] = ['CWID', 'Name', 'Dept', 'Course', 'Students']
+
+    def __init__(self, cwid: str, name: str, dept: str) -> None:
+
+        self._cwid: str = cwid
+        self._name: str = name
+        self._dept: str = dept"""" Test for Repository """
 
 import unittest
 import os
-from HW09_Dharmit_Viradia import Repository, Student, Instructor
+from HW10_Dharmit_Viradia import Repository, Student, Instructor, Major
+
 
 class TestRepository(unittest.TestCase):
     """ Test for repository """
 
     def setUp(self) -> None:
         """This methods allow you to define instructions that will be executed before and after each test method"""
-        self.test_path: str = "HW09_Test"
+        self.test_path: str = "HW10_Test"
         self.repo: Repository = Repository(self.test_path, False)
+
+    def test_majors(self):
+        """ Testing majors table"""
+        expected = [['SFEN', ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'],
+                     ['CS 501', 'CS 513', 'CS 545']],
+                    ['SYEN', ['SYS 612', 'SYS 671', 'SYS 800'],
+                     ['SSW 540', 'SSW 565', 'SSW 810']]]
+
+        calculated = [majors.info()
+                      for majors in self.repo._majors.values()]
+        self.assertEqual(expected, calculated)
 
     def test_student_attributes(self) -> None:
         """ Testing for student attributes """
-        expected = {'10103':  ['10103', 'Baldwin, C', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687']],
-                    '10115':  ['10115', 'Wyatt, X', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687']],
-                    '10172':  ['10172', 'Forbes, I', ['SSW 555', 'SSW 567']],
-                    '10175':  ['10175', 'Erickson, D', ['SSW 564', 'SSW 567', 'SSW 687']],
-                    '10183':  ['10183', 'Chapman, O', ['SSW 689']],
-                    '11399':  ['11399', 'Cordova, I', ['SSW 540']],
-                    '11461':  ['11461', 'Wright, U', ['SYS 611', 'SYS 750', 'SYS 800']],
-                    '11658':  ['11658', 'Kelly, P', ['SSW 540']],
-                    '11714':  ['11714', 'Morton, A', ['SYS 611', 'SYS 645']],
-                    '11788':  ['11788', 'Fuller, E', ['SSW 540']]}
 
-        calculated = {cwid: student.info()
-                      for cwid, student in self.repo._students.items()}
+        expected = [['10103', 'Baldwin, C', 'SFEN', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.44],
+                    ['10115', 'Wyatt, X', 'SFEN', ['CS 545', 'SSW 564',
+                                                   'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.81],
+                    ['10172', 'Forbes, I', 'SFEN', ['SSW 555', 'SSW 567'], [
+                        'SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], 3.88],
+                    ['10175', 'Erickson, D', 'SFEN', ['SSW 564', 'SSW 567', 'SSW 687'], [
+                        'SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], 3.58],
+                    ['10183', 'Chapman, O', 'SFEN', ['SSW 689'], ['SSW 540', 'SSW 555',
+                                                                  'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], 4.0],
+                    ['11399', 'Cordova, I', 'SYEN', ['SSW 540'], [
+                        'SYS 612', 'SYS 671', 'SYS 800'], [], 3.0],
+                    ['11461', 'Wright, U', 'SYEN', ['SYS 611', 'SYS 750', 'SYS 800'], [
+                        'SYS 612', 'SYS 671'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.92],
+                    ['11658', 'Kelly, P', 'SYEN', [], ['SYS 612', 'SYS 671',
+                                                       'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 0.0],
+                    ['11714', 'Morton, A', 'SYEN', ['SYS 611', 'SYS 645'], ['SYS 612',
+                                                                            'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.0],
+                    ['11788', 'Fuller, E', 'SYEN', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], 4.0]]
+
+        calculated = [student.info()
+                      for cwid, student in self.repo._students.items()]
         self.assertEqual(expected, calculated)
 
     def test_instructor_attributes(self) -> None:
@@ -52,3 +245,22 @@ class TestRepository(unittest.TestCase):
 if __name__ == "__main__":
     """ Run test cases on startup """
     unittest.main(exit=False, verbosity=2)
+
+        self._courses: DefaultDict[str, int] = defaultdict(int)
+
+    def add_student(self, course: str) -> None:
+        """ Number of students taking course with Instructor """
+        self._courses[course] += 1
+
+    def info(self) -> Iterator[Tuple[str, str, str, str, int]]:
+        """ Yield the row """
+        for course, count in self._courses.items():
+            yield [self._cwid, self._name, self._dept, course, count]
+
+
+def main():
+    Repository('HW10_Test')
+
+
+if __name__ == '__main__':
+    main()
